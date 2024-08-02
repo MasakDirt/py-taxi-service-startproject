a450a44c6c25dcdf9ab2c0fbee00e04ef3283542
maksym_korniev <maksymkorniev88@gmail.com>
2024-08-02 20:35:35 +0300

    started taxi service project

---
+++ taxi/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from taxi_service import settings
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.country})"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.username} {self.first_name} {self.last_name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cars",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.manufacturer.name} {self.model}"
